ae0d6d569524fc4e98cf4ed1d3be55d3648c2f8c
altitude <703362086@qq.com>
2017-07-07 23:01:45 +0800

    first commit

---
+++ two.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from KNN import * 
+from numpy import *   
+  
+dataSet, labels = KNN.createDataSet()  
+  
+testX = array([1.2, 1.0])  
+k = 3  
+outputLabel = KNN.kNNClassify(testX, dataSet, labels, 3)  
+print ("Your input is:", testX, "and classified to class: ", outputLabel ) 
+  
+testX = array([0.1, 0.3])  
+outputLabel = KNN.kNNClassify(testX, dataSet, labels, 3)  
+print ("Your input is:", testX, "and classified to class: ", outputLabel)
